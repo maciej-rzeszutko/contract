2e4d17822bbc2a44f9a9bfdfa1277c2e550c885d
maciej.rzeszutko <maciej.rzeszutko@adverity.com>
2022-10-18 09:35:36 +0200

    Fraudcheck generating contract test

---
+++ src/main/kotlin/com/example/contract/ContractApplication.kt
@@ -0,0 +1,11 @@
+package com.example.contract
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContractApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContractApplication>(*args)
+}
